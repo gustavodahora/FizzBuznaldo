74d09bbd13f44ab28c215fbac1f978e4aa16cfff
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2022-06-06 23:48:09 -0300

    repo sera deletado meus pais

--- src/MundialDoPalmeiras.kt
+++ src/MundialDoPalmeiras.kt
@@ -2,10 +2,13 @@ fun main() {
     for (i in 1..100) {
         if (i % 3 == 0 && i % 5 == 0) {
             println("FizzBuss")
+            continue
         } else if (i % 5 == 0) {
             println("buss")
+            continue
         } else if (i % 3 == 0) {
             println("fizz")
+            continue
         }
         println(i)
     }
